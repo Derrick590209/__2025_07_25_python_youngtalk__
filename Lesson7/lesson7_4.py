c63838bc714fef607f2478ad5b923a6ca89479ab
林宏璋 <152790820+Derrick590209@users.noreply.github.com>
2026-06-28 13:56:03 +0000

    Modify

--- Lesson7/lesson7_4.py
+++ Lesson7/lesson7_4.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="讓使用者輸入姓名")
-parser.add_argument("-n", "--name", type=str,help="姓名")
+parser.add_argument("-n", "--name", type=str,help="這裡要放姓名")
 args = parser.parse_args()
 
 if not args.name: #沒有輸入是None,所以not args.name是True
