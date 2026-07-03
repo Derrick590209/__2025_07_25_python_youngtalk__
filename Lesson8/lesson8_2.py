f17ebb5684c7a95a82a5769ea20357ea5527c2ba
林宏璋 <152790820+Derrick590209@users.noreply.github.com>
2026-07-03 14:34:28 +0000

    lesson8_2 modify

--- Lesson8/lesson8_2.py
+++ Lesson8/lesson8_2.py
@@ -1,31 +1,29 @@
-#猜數字遊戲
-import random
+import argparse
+#import game
+import tools
 
-def play_game():
-  print("======猜數字遊戲======")
-  min = 1
-  max = 100
-  count = 0
-  target = random.randint(min,max)
-  print(target)
-  while True:
-    #input("猜數字的範圍:",min,"~",max) # input只能有一個引數值 所以不對 更新如下
-    keyin = int(input(f"猜數字的範圍:{min}~{max}:"))
-    count += 1
-    if keyin >= min and keyin <= max:
-      if target == keyin:
-        print(f"賓果!猜對了,答案是:{target}")
-        print(f"您總共猜了{count}次\n")
-        break
-      elif keyin > target:
-        print(f"猜錯了!再小一點")
-        max = keyin - 1
+def parse_name()->str:
+    parser = argparse.ArgumentParser(description="猜數字遊戲")
+    parser.add_argument("-n", "--name",type=str, help = "姓名")
+    args = parser.parse_args()
 
-      else:
-        print(f"猜錯了!再大一點")
-        min = keyin + 1
-      print(f"您已經猜了{count}次\n")
+    if not args.name:
+        name = input("請輸入姓名:")
     else:
-      print("請輸入提示範圍內的數字\n")
-play_game()
-print("遊戲結束")
+        name = args.name
+    
+    return name
+
+def main():    
+    name:str = parse_name()
+    playCount:int = 0
+    while(True):
+        tools.game.play_game()
+        playCount += 1
+        play_again:str = input("您還要繼續嗎(y,n):")
+        if play_again == 'n':
+            break
+    print(f"{name}:遊戲結束,您共玩{playCount}次")
+
+if __name__ == '__main__':
+    main()
